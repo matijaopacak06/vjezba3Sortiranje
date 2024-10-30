9cb7f58ff36328947f3a87459ad3c284ad09ee70
matijaopacak06 <153612293+matijaopacak06@users.noreply.github.com>
2024-10-30 09:40:01 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_sortiranje
+{
+    internal class Program
+    {
+        class Student
+        {
+            public int StudentID { get; set; }
+            public string StudentName { get; set; }
+            public int Age { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Student[] studentArray = {
+            new Student() { StudentID = 1, StudentName = "John", Age = 18 },
+            new Student() { StudentID = 2, StudentName = "Steve",  Age = 21 },
+            new Student() { StudentID = 3, StudentName = "Bill",  Age = 25 },
+            new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 },
+            new Student() { StudentID = 5, StudentName = "Ron" , Age = 31 },
+            new Student() { StudentID = 6, StudentName = "Chris",  Age = 17 },
+            new Student() { StudentID = 7, StudentName = "Rob",Age = 19  },
+        };
+
+            var result = from s in studentArray
+                         where s.Age > 18 && s.Age < 26
+                         select s.Age;
+
+
+
+            int i = 0;
+
+            foreach (var std in studentArray)
+            { 
+                Console.WriteLine($"ID: {std.StudentID}, Name: {std.StudentName}, Age:{std.Age}");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
+    
+
